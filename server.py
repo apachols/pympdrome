0783a66cd4ee3b7edd1b6b53d30d58ea7717b6ab
Adam Pacholski <apachols@gmail.com>
2018-06-26 21:02:55 -0700

    Web UI is good enough

--- server.py
+++ server.py
@@ -8,6 +8,7 @@ from lib import radio, system
 import subprocess
 
 app = Flask(__name__)
+# app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 300
 
 def get_playlists():
     ls = subprocess.check_output(['ls', '/Users/adamp/Music/MPD'], stderr=subprocess.STDOUT)
@@ -18,7 +19,7 @@ playlists = get_playlists()
 @app.route("/")
 def index(playing=""):
     return render_template(
-        'index.html',
+        "index.html",
         playlists=playlists,
         playing=playing
     )
@@ -29,16 +30,16 @@ def next(playlist=None):
     if not playlist:
         playlist = system.getCurrentPlaylistName()
     radio.skipToNextSong()
-    return redirect('/playlist/{}'.format(playlist))
+    return launch(playlist)
 
 @app.route("/stop/")
 @app.route("/stop/<playlist>")
 def stop(playlist=""):
     radio.stopPlayback()
-    return redirect('/')
+    return index("")
 
-@app.route('/playlist/<playlist>')
-def playlist(playlist):
-    radio.smartHandleButtonPress(playlist)
+@app.route("/playlist/<playlist>")
+def launch(playlist):
+    radio.launchPlaylist(playlist)
     return index(playlist)
 
