fe21db0e274ed57a98ec475e187a69a3c7089c40
Adam Pacholski <apachols@gmail.com>
2019-06-11 19:16:53 -0700

    Oops more files

---
+++ loadplaylists.py
@@ -0,0 +1,24 @@
+import os
+import subprocess
+from lib import system
+from lib import radio
+from time import sleep
+
+ROOT_DIR = '/Users/adamp'
+ROOT_DIR = '/home'
+
+PLAYLISTDIR = '{}/Music/MPD'.format(ROOT_DIR)
+
+def get_playlists():
+    ls = subprocess.check_output(['ls', PLAYLISTDIR], stderr=subprocess.STDOUT)
+    return [x for x in ls.split('\n') if x]
+
+# RELOAD ALL PLAYLISTS
+for playlist in get_playlists():
+    print 'Reloading', '||' + playlist + '||', 'shuffle on'
+    shuffle = True
+    radio.loadPlaylistIntoMPC(playlist, True)
+    radio.launchPlaylist(playlist)
+    radio.stopPlayback()
+    sleep(1)
+
