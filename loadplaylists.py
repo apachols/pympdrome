9350456c913d892d2c2ad7c456e9bd1948d0874c
martypie <marten@metal-heart.org>
2019-07-07 01:55:03 +0000

    Upate loadplaylists to python3

--- loadplaylists.py
+++ loadplaylists.py
@@ -6,11 +6,11 @@ from time import sleep
 
 ROOT_DIR = '/home/pi'
 
-PLAYLISTDIR = '{}/Music/MPD'.format(ROOT_DIR)
+PLAYLISTDIR = '{}/Music'.format(ROOT_DIR)
 
 def get_playlists():
     ls = subprocess.check_output(['ls', PLAYLISTDIR], stderr=subprocess.STDOUT)
-    return [x for x in ls.split('\n') if x]
+    return [x for x in ls.decode().split('\n') if x]
 
 # RELOAD ALL PLAYLISTS
 for playlist in get_playlists():
